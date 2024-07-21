a73a48356b6c72924140c232748dde6793fa5a73
sohei.kakihara <souhei_kakihara@simple-inc.com>
2024-07-21 19:08:56 +0900

    init

---
+++ src/Controller/ItemsController.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Items Controller
+ *
+ * @method \App\Model\Entity\Item[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ItemsController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        // $items = $this->paginate($this->Items);
+        $items = [
+            'aaaaaaaaaaaaa',
+            'bbbbbbbbbbbbb',
+            'vvvvvvvvvvv',
+        ];
+        $this->set(compact('items'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Item id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $item = $this->Items->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set(compact('item'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $item = $this->Items->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $item = $this->Items->patchEntity($item, $this->request->getData());
+            if ($this->Items->save($item)) {
+                $this->Flash->success(__('The item has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The item could not be saved. Please, try again.'));
+        }
+        $this->set(compact('item'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Item id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $item = $this->Items->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $item = $this->Items->patchEntity($item, $this->request->getData());
+            if ($this->Items->save($item)) {
+                $this->Flash->success(__('The item has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The item could not be saved. Please, try again.'));
+        }
+        $this->set(compact('item'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Item id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $item = $this->Items->get($id);
+        if ($this->Items->delete($item)) {
+            $this->Flash->success(__('The item has been deleted.'));
+        } else {
+            $this->Flash->error(__('The item could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ templates/Items/index.php
@@ -0,0 +1,5 @@
+<ul>
+<?php foreach ($items as $item): ?>
+  <li><?= $item ?></li>
+<?php endforeach; ?>
+</ul>
